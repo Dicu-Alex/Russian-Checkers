1d6359937255578e7e1696bbf8a2f6070d2bb1a3
Kidda <n_anamaria00@yahoo.com>
2021-04-03 21:47:32 +0300

    Board

--- RussianCheckers/RussianCheckers/Form1.Designer.cs
+++ RussianCheckers/RussianCheckers/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.board = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.BackColor = System.Drawing.Color.Gray;
+            this.board.Location = new System.Drawing.Point(10, 10);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(880, 800);
+            this.board.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1098, 177);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1313, 1023);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.board);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Russian Checkers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel board;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- RussianCheckers/RussianCheckers/Form1.cs
+++ RussianCheckers/RussianCheckers/Form1.cs
@@ -15,7 +15,49 @@ namespace RussianCheckers
         public Form1()
         {
             InitializeComponent();
-            //Mergeeeeee
+        }
+
+        int n;
+
+        PictureBox[,] P;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = 8;
+            P = new PictureBox[n, n];
+            
+            int left = 2, top = 2;
+
+            Color[] colors = new Color[] {
+                Color.White, Color.Black
+            };
+
+            for (int i = 0; i < n; i++) {
+                left = 2;
+
+                if (i % 2 == 0) {
+                    colors[0] = Color.White;
+                    colors[1] = Color.Black;
+                }
+
+                else {
+                    colors[0] = Color.Black;
+                    colors[1] = Color.White;
+                }
+
+                for (int j = 0; j < n; j++) {
+                    P[i, j] = new PictureBox();
+                    P[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+                    P[i, j].Location = new Point(left, top);
+                    P[i, j].Size = new Size(82, 82);
+
+                    left += 82;
+
+                    board.Controls.Add(P[i, j]);
+                }
+
+                top += 82;
+            }
         }
     }
 }
